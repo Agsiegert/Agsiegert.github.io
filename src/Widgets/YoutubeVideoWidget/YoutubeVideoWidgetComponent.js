1c35a51a4564e57e8541f8b0da195cbd5410000e
Alexander Pepper <alexander.pepper@infopark.de>
2017-12-12 12:34:10 +0100

    YoutubeVideoWidget is now a react class.

--- src/Widgets/YoutubeVideoWidget/YoutubeVideoWidgetComponent.js
+++ src/Widgets/YoutubeVideoWidget/YoutubeVideoWidgetComponent.js
@@ -1,25 +1,33 @@
 import InPlaceEditingPlaceholder from 'Components/InPlaceEditingPlaceholder';
 
-Scrivito.provideComponent('YoutubeVideoWidget', ({ widget }) => {
-  const youtubeVideoId = widget.get('youtubeVideoId');
-
-  if (!youtubeVideoId) {
-    return (
-      <InPlaceEditingPlaceholder center={ true }>
-        Provide a YouTube video ID in the widget properties.
-      </InPlaceEditingPlaceholder>
-    );
+class YoutubeVideoWidgetComponent extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  return (
-    <div className="text-center">
-      <iframe
-        width="560"
-        height="315"
-        src={ `https://www.youtube.com/embed/${youtubeVideoId}` }
-        frameBorder="0"
-        gesture="media"
-      />
-    </div>
-  );
-});
+  render() {
+    const youtubeVideoId = this.props.widget.get('youtubeVideoId');
+
+    if (!youtubeVideoId) {
+      return (
+        <InPlaceEditingPlaceholder center={ true }>
+          Provide a YouTube video ID in the widget properties.
+        </InPlaceEditingPlaceholder>
+      );
+    }
+
+    return (
+      <div className="text-center">
+        <iframe
+          width="560"
+          height="315"
+          src={ `https://www.youtube.com/embed/${youtubeVideoId}` }
+          frameBorder="0"
+          gesture="media"
+        />
+      </div>
+    );
+  }
+}
+
+Scrivito.provideComponent('YoutubeVideoWidget', YoutubeVideoWidgetComponent);
