d9053f654f9422c4d955acb5d1db038f18a15e1a
Alexander Pepper <alexander.pepper@infopark.de>
2017-12-22 10:44:19 +0100

    Removed title from Video.
    This attribute is not used anywhere. Removing it.

--- src/Objs/Video/VideoEditingConfig.js
+++ src/Objs/Video/VideoEditingConfig.js
@@ -1,15 +1,11 @@
 Scrivito.provideEditingConfig('Video', {
   attributes: {
-    title: {
-      title: 'Title',
-    },
     tags: {
       title: 'Tags',
       description: 'Make it easier to find this video by adding some tags.',
     },
   },
   properties: [
-    'title',
     'tags',
   ],
 });
