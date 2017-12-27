a65cd2c68236e620bba87f0a06e9115cfe95234f
Antony Siegert <antony.siegert@infopark.de>
2017-12-27 11:32:01 +0100

    simplifies validations using html5 'required' validation only.

--- src/Widgets/ContactFormWidget/ContactFormWidgetComponent.js
+++ src/Widgets/ContactFormWidget/ContactFormWidgetComponent.js
@@ -1,129 +1,98 @@
 import './contactForm.html';
+/* The html file isneeded to Netlify Form handling, updates to inputs in this file should be
+added to the html filw as well.
+https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
+*/
 
-class ContactFormWidgetComponent extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      checkboxState: true,
-    };
-  }
-
-  onSubmit() {
-    this.preventDefault();
-  }
-
-  toggle() {
-    this.setState({
-      checkboxState: !this.state.checkboxState,
-    });
-  }
-
-  render() {
-    const widget = this.props.widget;
-    const isEnabled = !this.state.checkboxState;
-    const agreement = this.state.checkboxState
-      ? 'checkbox missing' : 'checkbox';
-    const reminder =
-      <p>
-        { this.state.checkboxState ? 'Please agree to text' : '' }
-      </p>;
-
-    return (
-      <div className="row floating-label">
-        <form method="post" onSubmit={ this.onSubmit.bind(this) }>
-          <input
-            type="hidden"
-            name="form-name"
-            value="contact"
-          />
-          <div className="hidden">
-              <label>Don’t fill this out: <input name="bot-field" /></label>
-          </div>
-          <div className="col-sm-6">
-            <div className="form-group">
-              <label htmlFor="contactName">Your name</label>
-              <input
-                className="form-control input-lg"
-                id="contactName"
-                name="contactName"
-                placeholder="Name"
-                type="text"
-              />
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="form-group">
-              <label htmlFor="contactFamilyName">Family name</label>
-              <input
-                className="form-control input-lg"
-                id="contactFamilyName"
-                name="contactFamilyName"
-                placeholder="Family name"
-                type="text"
-              />
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="form-group">
-              <label htmlFor="contactEmail">Email address</label>
-              <input
-                className="form-control input-lg"
-                id="contactEmail"
-                name="contactEmail"
-                placeholder="Email"
-                type="email"
-              />
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="form-group">
-              <label htmlFor="contactCompany">Company</label>
-              <input
-                className="form-control input-lg"
-                id="contactCompany"
-                name="contactCompany"
-                placeholder="Company"
-                type="text"
-              />
-            </div>
-          </div>
-          <div className="col-sm-12">
-            <div className="form-group">
-              <label htmlFor="contactMessage">Message</label>
-              <textarea
-                className="form-control input-lg"
-                rows="3"
-                id="contactMessage"
-                name="contactMessage"
-                placeholder="Your Message..."
-              />
-            </div>
-            {
-              widget.get('agreementText') && <div className={ agreement }>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="contactAgreement"
-                    value={ widget.get('agreementText') }
-                    onClick={ this.toggle.bind(this) }
-                  />
-                  { widget.get('agreementText') }
-                </label>
-                { reminder }
-              </div>
-            }
-            <button
-              disabled={ !isEnabled }
-              className="btn btn-primary btn-block"
-              type="submit">
-              { widget.get('buttonText') || 'send message' }
-            </button>
-          </div>
-        </form>
+Scrivito.provideComponent('ContactFormWidget', ({ widget }) =>
+  <div className="row floating-label">
+    <form method="post">
+      <input
+        type="hidden"
+        name="form-name"
+        value="contact"
+      />
+      <div className="hidden">
+          <label>Don’t fill this out: <input name="bot-field" /></label>
       </div>
-    );
-  }
-}
-
-Scrivito.provideComponent('ContactFormWidget', ContactFormWidgetComponent);
+      <div className="col-sm-6">
+        <div className="form-group">
+          <label htmlFor="contactName">Your name</label>
+          <input
+            className="form-control input-lg"
+            id="contactName"
+            name="contactName"
+            placeholder="Name"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="form-group">
+          <label htmlFor="contactFamilyName">Family name</label>
+          <input
+            className="form-control input-lg"
+            id="contactFamilyName"
+            name="contactFamilyName"
+            placeholder="Family name"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="form-group">
+          <label htmlFor="contactEmail">Email address</label>
+          <input
+            className="form-control input-lg"
+            id="contactEmail"
+            name="contactEmail"
+            placeholder="Email"
+            type="email"
+          />
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="form-group">
+          <label htmlFor="contactCompany">Company</label>
+          <input
+            className="form-control input-lg"
+            id="contactCompany"
+            name="contactCompany"
+            placeholder="Company"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="col-sm-12">
+        <div className="form-group">
+          <label htmlFor="contactMessage">Message</label>
+          <textarea
+            className="form-control input-lg"
+            rows="3"
+            id="contactMessage"
+            name="contactMessage"
+            placeholder="Your Message..."
+          />
+        </div>
+        {
+          widget.get('agreementText') && <div className="checkbox">
+            <label>
+              <input
+                type="checkbox"
+                name="contactAgreement"
+                value={ widget.get('agreementText') }
+                required
+              />
+              { widget.get('agreementText') }
+            </label>
+          </div>
+        }
+        <button
+          className="btn btn-primary btn-block"
+          type="submit">
+          { widget.get('buttonText') || 'send message' }
+        </button>
+      </div>
+    </form>
+  </div>
+);
--- src/Widgets/SignUpFormWidget/SignUpFormWidgetComponent.js
+++ src/Widgets/SignUpFormWidget/SignUpFormWidgetComponent.js
@@ -1,106 +1,69 @@
-class SignUpFormWidgetComponent extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      checkboxState: true,
-    };
+Scrivito.provideComponent('SignUpFormWidget', ({ widget }) => {
+  const classNames = [];
+  if (widget.get('isTransparent') === 'yes') {
+    classNames.push('panel-white-transparent');
+  } else {
+    classNames.push('panel');
+    classNames.push('panel-padding');
+    classNames.push('floating-label');
   }
 
-  onSubmit() {
-    this.preventDefault();
-  }
+  return (
+    <div className={ classNames.join(' ') }>
+      <form>
+        <Scrivito.ContentTag
+          content={ widget }
+          attribute="title"
+          tag="h1"
+          className="h3 text-center"
+        />
 
-  toggle() {
-    this.setState({
-      checkboxState: !this.state.checkboxState,
-    });
-  }
-
-  render() {
-    const widget = this.props.widget;
-    const isEnabled = !this.state.checkboxState;
-    const agreement = this.state.checkboxState
-      ? 'checkbox missing' : 'checkbox';
-    const reminder =
-      <span>
-        { this.state.checkboxState ? 'Please agree to text' : '' }
-      </span>;
-    const classNames = [];
-    if (widget.get('isTransparent') === 'yes') {
-      classNames.push('panel-white-transparent');
-    } else {
-      classNames.push('panel');
-      classNames.push('panel-padding');
-      classNames.push('floating-label');
-    }
-
-    return (
-      <div className={ classNames.join(' ') }>
-        <form onSubmit={ this.onSubmit.bind(this) }>
-          <Scrivito.ContentTag
-            content={ widget }
-            attribute="title"
-            tag="h1"
-            className="h3 text-center"
+        <div className="form-group">
+          <label htmlFor="signUpUsername">Your username</label>
+          <input
+            className="form-control input-lg"
+            id="signUpUsername"
+            placeholder="Username"
+            type="text"
           />
-          <div className="form-group">
-            <label htmlFor="signUpUsername">Your username</label>
-            <input
-              className="form-control input-lg"
-              id="signUpUsername"
-              placeholder="Username"
-              type="text"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="SignUpEmail">Your email</label>
-            <input
-              className="form-control input-lg"
-              id="SignUpEmail"
-              placeholder="alice@example.org"
-              type="email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="SignUpPassword1">Your password</label>
-            <input
-              className="form-control input-lg"
-              id="SignUpPassword1"
-              placeholder="Password"
-              type="password"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="SignUpPassword2">Confirm password</label>
-            <input
-              className="form-control input-lg"
-              id="SignUpPassword2"
-              placeholder="Confirm Password"
-              type="password"
-            />
-          </div>
-          <div className={ agreement }>
-            <label>
-              <input
-                type="checkbox"
-                value="true"
-                onClick={ this.toggle.bind(this) }
-              />
-              I read the licence agreement.
-            </label>
-            { reminder }
-          </div>
-          <button
-            disabled={ !isEnabled }
-            className="btn btn-primary btn-block"
-            type="submit">
-            { widget.get('buttonText') || 'Sign up now' }
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-
-Scrivito.provideComponent('SignUpFormWidget', SignUpFormWidgetComponent);
+        </div>
+        <div className="form-group">
+          <label htmlFor="SignUpEmail">Your email</label>
+          <input
+            className="form-control input-lg"
+            id="SignUpEmail"
+            placeholder="alice@example.org"
+            type="email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="SignUpPassword1">Your password</label>
+          <input
+            className="form-control input-lg"
+            id="SignUpPassword1"
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="SignUpPassword2">Confirm password</label>
+          <input
+            className="form-control input-lg"
+            id="SignUpPassword2"
+            placeholder="Confirm Password"
+            type="password"
+          />
+        </div>
+        <div className="checkbox">
+          <label>
+            <input value="" type="checkbox" />
+            I read the licence agreement.
+          </label>
+        </div>
+        <button className="btn btn-primary btn-block">
+          { widget.get('buttonText') || 'Sign up now' }
+        </button>
+      </form>
+    </div>
+  );
+});
