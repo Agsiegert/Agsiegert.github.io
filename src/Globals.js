8db59b14e6abd3034521f46e1542f820771f4c5c
Alexander Pepper <alexander.pepper@infopark.de>
2018-01-15 11:38:04 +0100

    scrivito_extensions.js now work again.
    This is a workaround for issue https://github.com/infopark/rails_connector/issues/3721.

--- src/Globals.js
+++ src/Globals.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import * as Scrivito from 'scrivito';
 
 window.React = React;
+window.ReactDOM = ReactDOM;
 window.Scrivito = Scrivito;
