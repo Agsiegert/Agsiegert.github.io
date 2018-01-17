2afafe5c3df789f7f1a87de106e8ea463636005c
Alexander Pepper <alexander.pepper@infopark.de>
2018-01-17 10:10:22 +0100

    Removed now obsolete window.ReactDOM

--- src/Globals.js
+++ src/Globals.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import * as Scrivito from 'scrivito';
 
 window.React = React;
-window.ReactDOM = ReactDOM;
 window.Scrivito = Scrivito;
